46409cfcf44909cbab59cefb63beef4496fe9fd0
Michael Hanselmann <hansmi@vshn.ch>
2016-06-23 16:56:23 +0200

    Add check for machine timestamp via Hawkular

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name="vshn_npo",
+      packages=["vshn_npo"],
+      )
+
+# vim: set sw=2 sts=2 et :
---
+++ vshn_npo/hawkular_client.py
@@ -0,0 +1,182 @@
+import argparse
+import itertools
+import logging
+import requests
+import time
+import urllib.parse
+
+
+# Retry request if one of these status codes was received
+_RETRY_STATUS_CODES = frozenset([
+  requests.codes.internal_server_error,
+  requests.codes.bad_gateway,
+  requests.codes.service_unavailable,
+  requests.codes.gateway_timeout,
+  requests.codes.insufficient_storage,
+  requests.codes.bandwidth_limit_exceeded,
+  ])
+
+
+def idquote(text):
+  return urllib.parse.quote(text, "")
+
+
+def _format_tags(tags):
+  return ','.join("{}:{}".format(key, val) for (key, val) in tags.items())
+
+
+class HawkularClient(object):
+  """A simplistic client for the REST API provided by Hawkular.
+
+  """
+  def __init__(self, endpoint=None, tenant=None):
+    self._endpoint = endpoint
+    self._tenant = tenant
+    self._token = None
+    self._timeout = None
+    self._retries = 1
+
+  @property
+  def endpoint(self):
+    return self._endpoint
+
+  @endpoint.setter
+  def endpoint(self, value):
+    self._endpoint = value
+
+  @property
+  def tenant(self):
+    return self._tenant
+
+  @tenant.setter
+  def tenant(self, value):
+    self._tenant = value
+
+  @property
+  def token(self):
+    return self._token
+
+  @token.setter
+  def token(self, value):
+    self._token = value
+
+  @property
+  def timeout(self):
+    return self._timeout
+
+  @timeout.setter
+  def timeout(self, value):
+    self._timeout = value
+
+  @property
+  def retries(self):
+    return self._retries
+
+  @retries.setter
+  def retries(self, value):
+    self._retries = value
+
+  def get(self, path, params=None, tags=None):
+    """Retrieve data behind given path.
+
+    :param path: string relative to API endpoint
+    :param params: None or dict with request parameters
+    :param tags: None or dict with request tags
+
+    """
+    if self._tenant is None:
+      raise Exception("Tenant was not set")
+
+    url = "{}/{}".format(self._endpoint.rstrip("/"), path.lstrip("/"))
+
+    headers = {
+        "Hawkular-Tenant": self._tenant,
+        }
+
+    if self._token:
+      headers["Authorization"] = "Bearer {}".format(self._token)
+
+    if params is None:
+      params = {}
+
+    if tags:
+      params["tags"] = _format_tags(tags)
+
+    for attempt in itertools.count(1):
+      if attempt > 1:
+        delay = (attempt - 1) ** 1.1
+        logging.debug("Sleeping %0.1f seconds before trying again", delay)
+        time.sleep(delay)
+
+      try:
+        req = requests.get(url, headers=headers, params=params, timeout=self._timeout)
+      except requests.exceptions.Timeout as err:
+        if attempt < self._retries:
+          logging.debug("Connection timed out: %s", err)
+          continue
+        raise
+
+      if req.status_code == 204:
+        # No content
+        return None
+
+      # Important: Only certain verbs are idempotent in HTTP, one of them being
+      # GET. That must be taken into consideration should this code ever be
+      # extended for other verbs.
+      if req.status_code in _RETRY_STATUS_CODES and attempt < self._retries:
+        logging.debug("Request failed with status code %s", req.status_code)
+        continue
+
+      # All other issues are fatal
+      req.raise_for_status()
+
+      return req.json()
+
+    # This should never be reached
+    raise Exception("Request failed after %s attempts", self._retries)
+
+
+def setup_argument_parser(parser):
+  """Add common arguments to an argument parser.
+
+  :param parser: An instance of :class:`argparse.ArgumentParser`
+
+  """
+  group = parser.add_argument_group(title="API client")
+  group.add_argument("--endpoint", required=True, metavar="URL",
+                     help="API endpoint")
+  group.add_argument("--tenant", default="_system", metavar="NAME",
+                     help="Hawkular tenant")
+
+  tokgroup = group.add_mutually_exclusive_group(required=False)
+  tokgroup.add_argument("--token", help="Bearer token for authentication")
+  tokgroup.add_argument("--token-from", metavar="PATH",
+                        type=argparse.FileType(mode="r"),
+                        help=("Path to file containing bearer token for"
+                              " authentication"))
+
+  group.add_argument("--timeout", type=int, default=10, metavar="SEC",
+                     help="Request timeout in seconds")
+  group.add_argument("--query-retries", type=int, default=3, metavar="N",
+                     help="Number of retries for GET queries")
+
+
+def make_client(args):
+  """Instantiate Hawkular client based on parsed arguments.
+
+  :param args: An instance of :class:`argparse.Namespace` as returned by
+    :func:`argparse.ArgumentParser.parse_args` and related functions
+
+  """
+  if args.token_from is not None:
+    token = args.token_from.read().rstrip()
+  else:
+    token = args.token
+
+  client = HawkularClient(args.endpoint, args.tenant)
+  client.token = token
+  client.timeout = args.timeout
+  client.retries = args.query_retries
+  return client
+
+# vim: set sw=2 sts=2 et :
