1b4f0b8d0e476bbcf16008ffddf201996e1cd618
Michael Hanselmann <hansmi@vshn.ch>
2016-11-02 10:10:55 +0100

    Remove workaround for Hawkular-Metrics before v0.8.2
    Hawkular-Metrics before version 0.8.2 did not export an object's type to
    the tags. Now that all clusters are using more recent versions this
    workaround can be removed.
    
    This removal has the intended side-effect of fixing an issue whereby
    empty metrics produced an error messages due to there not being a "tags"
    member (“KeyError: 'tags'”).

--- vshn_npo/hawkular_utils.py
+++ vshn_npo/hawkular_utils.py
@@ -65,32 +65,15 @@ class LabelFilter:
 
 
 def select_metrics(client, label_filter, tags):
-  # TODO: Remove workaround once Hawkular-Metrics 0.8.2 or newer is used on all
-  # clusters. Earlier versions do not have a "type" tag.
-  if "type" in tags:
-    # Avoid modifying caller's object
-    tags = tags.copy()
-    typetag = tags.pop("type")
+  """Retrieve and filter metrics based on a label filter and tags.
 
-    if typetag == "pod":
-      local_filter_fn = lambda metric: "container_name" not in metric["tags"]
-    elif typetag == "pod_container":
-      local_filter_fn = lambda metric: "container_name" in metric["tags"]
-    else:
-      raise Exception(("Workaround not implemented for type \"{}\"".
-                       format(typetag)))
-  else:
-    local_filter_fn = None
-
+  """
+  # TODO: Filter metrics directly in the query (difficult with Hawkular
+  # 0.8, hence not yet implemented)
   metrics = client.get("metrics", tags=tags)
 
   if metrics.data:
     for i in metrics.data:
-      if local_filter_fn and not local_filter_fn(i):
-        continue
-
-      # TODO: Filter metrics directly in the query (difficult with Hawkular
-      # 0.8, hence not yet implemented)
       if label_filter.want_metric(i):
         yield i
 
